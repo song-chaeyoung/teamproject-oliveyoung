15a98b245ae1e76a1f6e3d2f1f5baca6f4b9ff4f
song-chaeyoung <call0512@naver.com>
2024-08-01 17:27:03 +0900

    first common

---
+++ component/header.js
@@ -0,0 +1,21 @@
+// Header Fixed
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+
+  if (window.scrollY > 100) header.classList.add("active");
+  else header.classList.remove("active");
+});
+
+// Category Evnet
+const categoryBtn = document.querySelector(".header .header_left_category");
+
+categoryBtn.addEventListener("click", () => {
+  console.log("click");
+  const category = document.querySelector(".haeder_category_container");
+  const categoryXmark = category.querySelector(".haader_category_xmark");
+
+  category.classList.toggle("active");
+  categoryXmark.addEventListener("click", () => {
+    category.classList.remove("active");
+  });
+});
