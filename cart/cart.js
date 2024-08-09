25a480ea1304fe625b1f9ff82cc071c66156a26b
dghasdf <az3014@naver.com>
2024-08-09 19:01:07 +0900

    kjy 240809

--- cart/cart.js
+++ cart/cart.js
@@ -48,11 +48,9 @@ tabs.forEach((tab, i) => {
 });
 // const tab1 = document.querySelector(".tab1");
 // const tab2 = document.querySelector(".tab2");
-// const tab3 = document.querySelector(".tab3");
 
 // const content1 = document.querySelector(".content1");
 // const content2 = document.querySelector(".content2");
-// const content3 = document.querySelector(".content3");
 
 // tab1.addEventListener("click", function () {
 //   this.classList.add("active");
@@ -60,7 +58,6 @@ tabs.forEach((tab, i) => {
 //   tab3.classList.remove("active");
 //   content1.classList.add("active");
 //   content2.classList.remove("active");
-//   content3.classList.remove("active");
 // });
 
 // tab2.addEventListener("click", function () {
@@ -69,14 +66,12 @@ tabs.forEach((tab, i) => {
 //   tab3.classList.remove("active");
 //   content2.classList.add("active");
 //   content1.classList.remove("active");
-//   content3.classList.remove("active");
 // });
 
 // tab3.addEventListener("click", function () {
 //   this.classList.add("active");
 //   tab1.classList.remove("active");
 //   tab2.classList.remove("active");
-//   content3.classList.add("active");
+
 //   content1.classList.remove("active");
-//   content2.classList.remove("active");
 // });
