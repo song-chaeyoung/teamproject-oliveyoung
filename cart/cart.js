7adcb9b2b12420563ce951efd9f3d8774f7a6f5f
dghasdf <az3014@naver.com>
2024-08-19 17:51:58 +0900

    kjy 240819

--- cart/cart.js
+++ cart/cart.js
@@ -30,7 +30,9 @@ fetch("/component/footer.html")
 
 // tab
 const tabs = document.querySelectorAll(".tabs li");
-const contents = document.querySelectorAll(".contents .content1");
+const contents = document.querySelectorAll(
+  ".contents .content1, .contents .content2"
+);
 
 tabs.forEach((tab, i) => {
   tab.addEventListener("click", () => {
@@ -48,11 +50,9 @@ tabs.forEach((tab, i) => {
 });
 // const tab1 = document.querySelector(".tab1");
 // const tab2 = document.querySelector(".tab2");
-// const tab3 = document.querySelector(".tab3");
 
 // const content1 = document.querySelector(".content1");
 // const content2 = document.querySelector(".content2");
-// const content3 = document.querySelector(".content3");
 
 // tab1.addEventListener("click", function () {
 //   this.classList.add("active");
@@ -60,7 +60,6 @@ tabs.forEach((tab, i) => {
 //   tab3.classList.remove("active");
 //   content1.classList.add("active");
 //   content2.classList.remove("active");
-//   content3.classList.remove("active");
 // });
 
 // tab2.addEventListener("click", function () {
@@ -69,14 +68,12 @@ tabs.forEach((tab, i) => {
 //   tab3.classList.remove("active");
 //   content2.classList.add("active");
 //   content1.classList.remove("active");
-//   content3.classList.remove("active");
 // });
 
 // tab3.addEventListener("click", function () {
 //   this.classList.add("active");
 //   tab1.classList.remove("active");
 //   tab2.classList.remove("active");
-//   content3.classList.add("active");
+
 //   content1.classList.remove("active");
-//   content2.classList.remove("active");
 // });
