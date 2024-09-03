83c47eb2e0b176ea97d123fdaed286b99a27b191
onulen <skwid17@naver.com>
2024-09-04 00:40:54 +0900

    slickjsj0904

--- product/productDetail.js
+++ product/productDetail.js
@@ -348,11 +348,25 @@ function validateForm() {
 }
 
 // Slick Slider
-$('.autoplay').slick({
-  dots: false,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  infinite: true,
-  autoplaySpeed: 1000,
+$(document).ready(function(){
+  $('.productItemsList').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    infinite: true,
+    autoplaySpeed: 1000,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+    ]
+  });
 });
